187a66abc25053d7a106176b1eaefb47ab76e990
Ogolknev Nikita <ogolknev257@mail.ru>
2023-08-23 13:12:31 +0300

    add the main page template: a lot of changes

--- task_planner/main/static/main/js/main.js
+++ task_planner/main/static/main/js/main.js
@@ -4,36 +4,81 @@ function sleep(ms) {
 
 
 document.getElementsByClassName('menu-icon')[0].addEventListener('click', openMenu);
+document.getElementsByClassName('folder-arrow')[0].addEventListener('click', openFolders);
+document.getElementsByClassName('step-checkbox')[0].addEventListener('click', comply)
+
+menu_block = document.getElementById('menu-block');
+menu_icon = document.getElementById('menu-icon');
+menu_line_1 = document.getElementById('menu-line-1');
+menu_line_2 = document.getElementById('menu-line-2');
+menu_line_3 = document.getElementById('menu-line-3');
+title = document.getElementById('title');
+folders_section = document.getElementById('folder-section');
+folder_head = document.getElementById('folder-head');
+folder_arrow = document.getElementById('folder-arrow');
+folder_btns = document.getElementById('folder-btns')
+task_section = document.getElementById('task-section');
+step_block = document.getElementById('steps-block');
+
+
 
 async function openMenu(){
 
-    menu_block = document.getElementById('menu-block');
     menu_block_class = menu_block.getAttribute('class');
-    menu_icon = document.getElementById('menu-icon');
-    menu_line_1 = document.getElementById('menu-line-1');
-    menu_line_2 = document.getElementById('menu-line-2');
-    menu_line_3 = document.getElementById('menu-line-3');
-    title = document.getElementById('title');
 
     if(menu_block_class == "menu-block"){
         menu_block.className = "menu-block-on";
         setTimeout(function(){
             menu_icon.className = "menu-icon-on";
-        }, 50);
-        menu_line_1.className = 'line-1-on';
-        menu_line_2.className = 'line-2-on';
-        menu_line_3.className = 'line-3-on';
+        }, 0);
+        menu_line_1.className = 'menu-line-1-on';
+        menu_line_2.className = 'menu-line-2-on';
+        menu_line_3.className = 'menu-line-3-on';
         title.className = 'title-on';
     } 
-    else if(menu_block_class == "menu-block-on"){
+    else{
         setTimeout(function(){
             menu_block.className = "menu-block";
-        }, 50);
+        }, 0);
         menu_icon.className = "menu-icon";
-        menu_line_1.className = 'line-1';
-        menu_line_2.className = 'line-2';
-        menu_line_3.className = 'line-3';
+        menu_line_1.className = 'menu-line-1';
+        menu_line_2.className = 'menu-line-2';
+        menu_line_3.className = 'menu-line-3';
         title.className = 'title';
+    }
+
+}
+
+function openFolders(){
+
+    folders_section_class = folders_section.getAttribute('class');
+    
+    if(folders_section_class == 'folder-section'){
+        folders_section.className = 'folder-section-on';
+        folder_head.className = 'folder-head-on';
+        folder_arrow.className = 'folder-arrow-on';
+        folder_btns.className = 'folder-btns-on';
+        task_section.className = 'task-section-on';
+    }
+    else{
+        folders_section.className = 'folder-section';
+        folder_head.className = 'folder-head';
+        folder_arrow.className = 'folder-arrow';
+        folder_btns.className = 'folder-btns';
+        task_section.className = 'task-section';
+    }
+
+}
+
+function comply(){
+
+    step_block_class = step_block.getAttribute('class');
+
+    if(step_block_class == 'steps-block'){
+        step_block.className = 'steps-block-on';
+    }
+    else{
+        step_block.className = 'steps-block';
     }
 
 }
--- task_planner/main/views.py
+++ task_planner/main/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.template.defaulttags import register
+
+@register.filter
+def get_range(value):
+    return range(value)
+
+@register.filter
+def get_parity(value):
+    return not value % 2
 
 # Create your views here.
 def index(request):
